7396f55e656bb72b4157dcc61bd9609e796b8a08
gautamranjan09 <gautamranjan96@gmail.com>
2024-12-06 14:02:52 +0530

    "Add dark mode feature and search functionality
    - Implemented dark mode toggle with styling updates.
    - Added search functionality to filter content dynamically.
    - Updated UI components to support new features."
    has context menu

--- src/components/navBar/DarkMode.jsx
+++ src/components/navBar/DarkMode.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import LightButton from "../../assets/darkmode/light-mode-button.png";
+import DarkButton from "../../assets/darkmode/dark-mode-button.png";
 
 const DarkMode = () => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light");
+
+  const element = document.documentElement; // access to htm element(the root element of the document)
+
+  //set theme to localStorage and html root element
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    if (theme === "dark") {
+      element.classList.add("dark");
+    } else {
+      element.classList.remove("dark");
+    }
+  });
   return (
-    <div>
-      <div>
-        <p>darkmode</p>
-      </div>
+    <div className="relative">
+      <img onClick={() => setTheme(theme === "dark" ? "light" : "dark")} src={LightButton} alt="lightmode" className={`w-12 cursor-pointer absolute right-0 z-10 ${theme === "dark" ? "opacity-0" : "opacity-100"} transition-all duration-300`} />
+      <img onClick={() => setTheme(theme === "dark" ? "light" : "dark")} src={DarkButton} alt="darkmode" className={"w-12 cursor-pointer"} />
     </div>
-  )
-}
+  );
+};
 
 export default DarkMode;
--- src/components/navBar/SearchBar.jsx
+++ src/components/navBar/SearchBar.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState } from "react";
+import { IoMdSearch } from "react-icons/io";
+
+// Search Data
+const searchData = [
+  {
+    name: "Fashion",
+    image: "https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg",
+  },
+  {
+    name: "Shirt",
+    image: "https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg",
+  },
+  {
+    name: "Jacket",
+    image: "https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg",
+  },
+  {
+    name: "Mobile",
+    image: "https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg",
+  },
+  {
+    name: "Laptop",
+    image: "https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg",
+  },
+  {
+    name: "Home",
+    image: "https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg",
+  },
+  {
+    name: "book",
+    image: "https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg",
+  },
+];
 
 const SearchBar = () => {
+  //Search State
+  const [search, setSearch] = useState("");
+
+  //Filter search data
+  const filterSearchData = searchData.filter((obj) => obj.name.toLowerCase().includes(search)).slice(0, 8);
+
   return (
-    <div>SearchBar</div>
-  )
-}
+    <>
+      {/* serach input */}
+      <div className="relative group hidden sm:block">
+        <input type="text" placeholder="Search" onChange={(e) => setSearch(e.target.value)} className="search-bar focus:w-[300px] focus:pr-10 focus:text-[#f42c37] focus:dark:text-[#f42c37] duration-200" />
+        <IoMdSearch className="text-xl text-gray-600 group-hover:text-[#f42c37] dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200" />
+      </div>
+      {/* search drop-down */}
+      <div className="flex justify-center">
+        {search && (
+          <div className="absolute block right-14 top-14 bg-gray-200 w-96 md:w-96 lg:w-96 z-50 my-1 rounded-lg px-2 py-2 dark-text-black">
+            {filterSearchData.length > 0 ? (
+              <>
+                {filterSearchData.map((item, index) => {
+                  return (
+                    <div key={index} className="py-2 px-2">
+                      <div className="flex items-center gap-2">
+                        <img src={item.image} alt="image" className="w-10" />
+                        {item.name}
+                      </div>
+                    </div>
+                  );
+                })}
+              </>
+            ) : (
+              <>
+                <div className="flex justify-center">
+                  <img className=" w-20" src="https://cdn-icons-png.flaticon.com/128/10437/10437090.png" alt="img" />
+                </div>
+              </>
+            )}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default SearchBar;
