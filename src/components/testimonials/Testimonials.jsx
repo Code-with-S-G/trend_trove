49bf00c9f206662005a64d36b77b82af9d46853c
SwayamprabhaGitHub <pswayamprabha@gmail.com>
2024-12-17 21:58:33 +0530

    feat: Add Footer component and enhance Testimonial component with dark mode styles

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -74,13 +74,13 @@ const Testimonials = () => {
     ],
   };
   return (
-    <div className="py-10">
+    <div className="py-10 dark:bg-gray-900">
       <div className="container">
         {/* header section */}
         <div className="text-center mb-10 max-w-[600px] mx-auto">
           <p className="text-sm text-amber-400">What Our Customers Say</p>
-          <h1 className="text-3xl font-bold text-gray-900">Testimonials</h1>
-          <p className="text-xs text-gray-700">
+          <h1 className="text-3xl font-bold text-gray-900 dark:text-white">Testimonials</h1>
+          <p className="text-xs text-gray-400">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.
           </p>
         </div>
@@ -88,9 +88,8 @@ const Testimonials = () => {
         <div>
           <Slider {...settings}>
             {testimonialData.map((testimonial) => (
-              <div className='my-6'>
+              <div key={testimonial.id} className='my-6'>
                 <div
-                  key={testimonial.id}
                   className="flex flex-col gap-4 shadow-lg py-8 px-6 mx-4 rounded-xl dark:bg-gray-800 bg-amber-500/10 relative"
                 >
                   <div className="mb-4">
