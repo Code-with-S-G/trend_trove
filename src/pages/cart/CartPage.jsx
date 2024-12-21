b85e1c3322287d5da943eeaf007a9efd459d7c37
gautamranjan09 <gautamranjan96@gmail.com>
2024-12-21 15:15:22 +0530

    bug fix

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Layout from "../../components/layout/Layout";
 import { Trash } from "lucide-react";
+import Layout from "@/components/Layout/Layout";
 
 const products = [
   {
