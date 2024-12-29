331a7b5d52d65d2479e389fe168839cfab2ed85f
gautamranjan09 <gautamranjan96@gmail.com>
2024-12-29 21:57:27 +0530

    feat: integrated dynamic data into AllProduct and HomepageProductCard components

--- src/components/homePageProductCard/HomePageProductCard.jsx
+++ src/components/homePageProductCard/HomePageProductCard.jsx
@@ -1,83 +1,13 @@
-import React from 'react';
+import myContext from '@/context/myContext';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-const productData = [
-  {
-    id: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-    image: 'https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg',
-    title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-    desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-    price: 150,
-    trendingProductName: 'Featured',
-    quantity: 1,
-  },
-  {
-    id: 'Kaushalam kalash Copper Pot',
-    image: 'https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg',
-    title: 'Kaushalam kalash Copper Pot',
-    desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-    price: 120,
-    trendingProductName: 'Featured',
-    quantity: 1,
-  },
-  {
-    id: 'Hand Painted Blue Kaushalam Tea Pot in',
-    image: 'https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg',
-    title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-    desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-    price: 130,
-    trendingProductName: 'Featured',
-    quantity: 1,
-  },
-  {
-    id: 'Hand Painted Blue Kaushalam Tea Pot',
-    image: 'https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg',
-    title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-    desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-    price: 120,
-    trendingProductName: 'Featured',
-    quantity: 1,
-  },
-  {
-    id: 'Hand Painted Blue Kaushalam Tea',
-    image: 'https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg',
-    title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-    desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-    price: 150,
-    trendingProductName: 'Featured',
-    quantity: 1,
-  },
-  {
-    id: 'Kaushalam kalast',
-    image: 'https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg',
-    title: 'Kaushalam kalash Copper Pot',
-    desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-    price: 120,
-    trendingProductName: 'Featured',
-    quantity: 1,
-  },
-  {
-    id: 'Hand Painted Blue Kaushalam',
-    image: 'https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg',
-    title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-    desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-    price: 130,
-    trendingProductName: 'Featured',
-    quantity: 1,
-  },
-  {
-    id: 'Hand Painted',
-    image: 'https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg',
-    title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-    desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-    price: 120,
-    trendingProductName: 'Featured',
-    quantity: 1,
-  },
-];
+import { PropagateLoader } from 'react-spinners';
 
 const HomePageProductCard = () => {
   const navigate = useNavigate();
+  const context = useContext(myContext);
+  const { getAllProduct, loading } = context;
+
   return (
     <div className="bg-slate-100 dark:bg-slate-900 pt-5 mt-10">
       {/* heading */}
@@ -86,13 +16,27 @@ const HomePageProductCard = () => {
       {/* main */}
       <section className="text-gray-600 body-font ">
         <div className="container px-5 py-5 mx-auto">
+          <div className="text-center">{loading && <PropagateLoader color="#ec4899" />}</div>
           <div className="flex flex-wrap -m-4">
-            {productData.map((item, index) => {
-              const { image, title, price, id } = item;
+            {getAllProduct.slice(0, 8).map((item) => {
+              const { images, title, price, id } = item;
               return (
                 <div key={id} className="p-4 w-full md:w-1/4">
                   <div className="border border-gray-300 h-full rounded-xl overflow-hidden shadow-md cursor-pointer">
-                    <img onClick={() => navigate('/productinfo')} src={image} className="lg:h-80 h-96 w-full" alt="blog" />
+                    {images?.length > 0 ? (
+                      <img onClick={() => navigate('/productinfo')} src={images[0]} className="lg:h-80 h-96 w-full" alt="blog" />
+                    ) : (
+                      <div className="lg:h-80 h-96 w-full flex items-center justify-center bg-gray-200 dark:bg-gray-700">
+                        <svg className="w-24 h-24 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                          />
+                        </svg>
+                      </div>
+                    )}
                     <div className="p-6 dark:bg-gray-900/80">
                       <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 dark:text-gray-300/80 mb-1">TrendTrove</h2>
                       <h1 onClick={() => navigate('/productinfo')} className="title-font text-lg font-medium text-gray-900 mb-6 line-clamp-1 dark:text-gray-300/90">
@@ -100,7 +44,7 @@ const HomePageProductCard = () => {
                       </h1>
                       <div className="flex justify-between  items-center">
                         <h1 onClick={() => navigate('/productinfo')} className="title-font text-lg font-medium text-gray-900 dark:text-gray-300/90">
-                          ₹{price}
+                          ₹{parseFloat(price).toLocaleString()}
                         </h1>
                         <button className="bg-yellow-500 text-lg hover:bg-amber-500  text-white py-[5px] rounded-lg font-semibold px-3">Add To Cart</button>
                       </div>
--- src/pages/allproduct/AllProduct.jsx
+++ src/pages/allproduct/AllProduct.jsx
@@ -1,115 +1,67 @@
-import Layout from "@/components/Layout/Layout";
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import Layout from '@/components/Layout/Layout';
+import myContext from '@/context/myContext';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PropagateLoader } from 'react-spinners';
 
-const productData = [
-    {
-        id: "product1",
-        image: 'https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg',
-        title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-        desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-        price: 150,
-        trendingProductName: 'Featured',
-        quantity: 1,
-    },
-    {
-        id: "product2",
-        image: 'https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg',
-        title: 'Kaushalam kalash Copper Pot',
-        desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-        price: 120,
-        trendingProductName: 'Featured',
-        quantity: 1,
-    },
-    {
-        id: "product3",
-        image: 'https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg',
-        title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-        desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-        price: 130,
-        trendingProductName: 'Featured',
-        quantity: 1,
-    },
-    {
-        id: "product4",
-        image: 'https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg',
-        title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-        desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-        price: 120,
-        trendingProductName: 'Featured',
-        quantity: 1,
-    },
-    {
-        id: "product5",
-        image: 'https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg',
-        title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-        desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-        price: 150,
-        trendingProductName: 'Featured',
-        quantity: 1,
-    },
-    {
-        id: "product6",
-        image: 'https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg',
-        title: 'Kaushalam kalash Copper Pot',
-        desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-        price: 120,
-        trendingProductName: 'Featured',
-        quantity: 1,
-    },
-    {
-        id: "product7",
-        image: 'https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg',
-        title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-        desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-        price: 130,
-        trendingProductName: 'Featured',
-        quantity: 1,
-    },
-    {
-        id: "product8",
-        image: 'https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg',
-        title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-        desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-        price: 120,
-        trendingProductName: 'Featured',
-        quantity: 1,
-    }
-]
 const AllProduct = () => {
-    const navigate = useNavigate();
-    return <Layout>
-        <div className="bg-slate-100 dark:bg-[#2c2c2c] pt-5">
-      {/* heading */}
-      <h1 className="text-center mb-2 text-2xl font-bold">All Products</h1>
+  const navigate = useNavigate();
+  const context = useContext(myContext);
+  const { getAllProduct, loading } = context;
 
-      {/* main */}
-      <section className="text-gray-600 body-font ">
-        <div className="container px-5 py-5 mx-auto">
-          <div className="flex flex-wrap -m-4">
-            {productData.map((item) => {
-              const { image, title, price, id } = item;
-              return (
-                <div key={id} className="p-4 w-full md:w-1/4">
-                  <div className="border border-gray-300 h-full rounded-xl overflow-hidden shadow-md cursor-pointer">
-                    <img onClick={() => navigate('/productinfo')} src={image} className="lg:h-80 h-96 w-full" alt="blog" />
-                    <div className="p-6 dark:bg-gray-900/80">
-                      <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 dark:text-gray-300/80 mb-1">TrendTrove</h2>
-                      <h1 onClick={() => navigate('/productinfo')} className="title-font text-lg font-medium text-gray-900 mb-6 line-clamp-1 dark:text-gray-300/90">{title}</h1>
-                      <div className="flex justify-between  items-center">
-                        <h1 onClick={() => navigate('/productinfo')} className="title-font text-lg font-medium text-gray-900 dark:text-gray-300/90">₹{price}</h1>
-                        <button className="bg-yellow-500 text-lg hover:bg-amber-500  text-white py-[5px] rounded-lg font-semibold px-3">Add To Cart</button>
+  return (
+    <Layout>
+      <div className="bg-slate-100 w-full dark:bg-[#2c2c2c] pt-5">
+     
+        {/* heading */}
+        <h1 className="text-center mb-2 text-2xl font-bold">All Products</h1>
+
+        {/* main */}
+        <section className="text-gray-600 body-font ">
+          <div className="container px-5 py-5 mx-auto">
+          <div className="text-center">{loading && <PropagateLoader color="#ec4899" />}</div>
+            <div className="flex flex-wrap -m-4">
+              {getAllProduct.map((item) => {
+                const { images, title, price, id } = item;
+                return (
+                  <div key={id} className="p-4 w-full md:w-1/4">
+                    <div className="border border-gray-300 h-full rounded-xl overflow-hidden shadow-md cursor-pointer">
+                      {images?.length > 0 ? (
+                        <img onClick={() => navigate('/productinfo')} src={images[0]} className="lg:h-80 h-96 w-full" alt="blog" />
+                      ) : (
+                        <div className="lg:h-80 h-96 w-full flex items-center justify-center bg-gray-200 dark:bg-gray-700">
+                          <svg className="w-24 h-24 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                            />
+                          </svg>
+                        </div>
+                      )}
+                      <div className="p-6 dark:bg-gray-900/80">
+                        <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 dark:text-gray-300/80 mb-1">TrendTrove</h2>
+                        <h1 onClick={() => navigate('/productinfo')} className="title-font text-lg font-medium text-gray-900 mb-6 line-clamp-1 dark:text-gray-300/90">
+                          {title}
+                        </h1>
+                        <div className="flex justify-between  items-center">
+                          <h1 onClick={() => navigate('/productinfo')} className="title-font text-lg font-medium text-gray-900 dark:text-gray-300/90">
+                            ₹{parseFloat(price).toLocaleString()}
+                          </h1>
+                          <button className="bg-yellow-500 text-lg hover:bg-amber-500  text-white py-[5px] rounded-lg font-semibold px-3">Add To Cart</button>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
     </Layout>
-}
+  );
+};
 
 export default AllProduct;
